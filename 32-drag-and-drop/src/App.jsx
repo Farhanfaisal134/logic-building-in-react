2d73ebed240f266c06c361f9aeda40f7657531b9
Farhan <o3190474834@gmail.com>
2024-11-08 10:04:37 +0500

    drag&drop-done

---
+++ 32-drag-and-drop/src/App.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [todos, setTodos] = useState([]);
+
+  const fetchListOfTodos = async () => {
+    setLoading(true);
+    try {
+      const apiResponse = await fetch("https://dummyjson.com/todos?limit=5&skip=0");
+      const result = await apiResponse.json();
+      if (result?.todos?.length > 0) {
+        const updatedTodos = result.todos.map((todoItem) => ({
+          ...todoItem,
+          status: "wip",
+        }));
+        setTodos(updatedTodos);
+      }
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchListOfTodos();
+  }, []);
+
+  const onDragStart = (event, id) => {
+    event.dataTransfer.setData("id", id);
+  };
+
+  const onDrop = (event, status) => {
+    event.preventDefault();
+    const id = event.dataTransfer.getData("id");
+    setTodos((prevTodos) =>
+      prevTodos.map((todoItem) =>
+        todoItem.id.toString() === id ? { ...todoItem, status } : todoItem
+      )
+    );
+  };
+
+  const renderTodos = (status) => {
+    return todos
+      .filter((todoItem) => todoItem.status === status)
+      .map((todoItem) => ( 
+        <div
+          onDragStart={(event) => onDragStart(event, todoItem.id)}
+          draggable
+          key={todoItem.id}
+          className="px-4 py-2 bg-yellow-400 rounded-sm shadow-sm text-xl font-bold"
+        >
+          {todoItem.todo}
+        </div>
+      ));
+  };
+
+  return (
+    <div className="w-full text-center mt-10">
+      <h1 className="text-2xl font-bold">Drag and Drop</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="max-w-3xl mx-auto grid grid-cols-2 gap-4 mt-6 p-4 h-[80vh]">
+          <div
+            onDrop={(event) => onDrop(event, "wip")}
+            onDragOver={(event) => event.preventDefault()}
+            className="flex flex-col gap-4 border p-4"
+          >
+            <h3 className="text-2xl font-bold">In Progress</h3>
+            {renderTodos("wip")}
+          </div>
+          <div
+            onDrop={(event) => onDrop(event, "completed")}
+            onDragOver={(event) => event.preventDefault()}
+            className="flex flex-col gap-4 border p-4"
+          >
+            <h3 className="text-2xl font-bold">Completed</h3>
+            {renderTodos("completed")}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
